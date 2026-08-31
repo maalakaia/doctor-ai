fe0434da71bce168f7014a1fc731e419c8854c0e
maalakaia <jbohkar@gmail.com>
2026-08-31 18:10:25 +0000

    connect backend to openai api

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,36 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from openai import OpenAI
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
 
 app = FastAPI()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise RuntimeError("OPENAI_API_KEY is not set.")
+
+client = OpenAI(api_key=api_key)
 
 
 @app.get("/")
 def root():
     return {"message": "Doctor AI backend is working!"}
+
+
+@app.post("/chat")
+def chat(message: str):
+    response = client.responses.create(
+        model="gpt-5-mini",
+        input=message
+    )
+
+    return {
+        "response": response.output_text
+    }
