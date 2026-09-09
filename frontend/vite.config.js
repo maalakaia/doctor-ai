1d857d35c90c4d30e35879d61aae20b792f760d2
maalakaia <jbohkar@gmail.com>
2026-09-09 16:56:37 +0000

    fixed the stupid friggin mess i made when tryna fix the website when it was ac just hte wifi and there was actually nothing so lowk this commit is basically fucking nothing but whatever

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    host: "0.0.0.0",
+
+    allowedHosts: [
+      "ideal-space-fishstick-q7g55v54wqp7c49v9-5173.app.github.dev",
+    ],
+
     proxy: {
       "/api": {
         target: "http://localhost:8000",
